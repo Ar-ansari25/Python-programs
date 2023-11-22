0f22c0148f9d48009762cfd5f07e482cec83709c
Ar-ansari25 <ar50871@gmail.com>
2023-11-23 03:45:51 +0530

    squarefileadd

--- calculate-square/square.py
+++ calculate-square/square.py
@@ -1,4 +1,8 @@
 #solution 1(using exponential)
-num1 =int(input("Entera number Here:"))
-sr=num1**(1/2)
-print("The square root of given Number is:",sr)
+
+
+#solution 2 (Using math module)
+import math
+num= int(input("Enter a Number Here:"))
+sr= math.sqrt(num)
+print("the square root of given Number is:", sr)
---
+++ calculate-square/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+num1 =int(input("Entera number Here:"))
+# sr=num1**(1/2)
+# print("The square root of given Number is:",sr)
